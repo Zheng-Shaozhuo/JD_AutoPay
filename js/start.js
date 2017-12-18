fe53e6356fe2d0ffd364afe74631f83691ed6fb2
Shaozhuo <Zheng-Shaozhuo@outlook.com>
2017-12-18 12:10:45 +0800

    预约抢购是否自动刷新当前页面功能

--- js/start.js
+++ js/start.js
@@ -3,26 +3,73 @@ document.addEventListener('DOMContentLoaded', function() {
 		console.log(result.isAuto);
 		if (10 == result.isAuto) {
 			document.getElementById('checbox').checked = true;
+			document.getElementById('zidong').style.display = 'block';
 		} else {
 			document.getElementById('checbox').checked = false;
+			document.getElementById('zidong').style.display = 'none';
+		}
+	});
+	chrome.storage.local.get('isAutoRe', function(result) {
+		console.log(result.isAutoRe);
+		if (10 == result.isAutoRe) {
+			document.getElementById('autoRechecbox').checked = true;
+		} else {
+			document.getElementById('autoRechecbox').checked = false;
+		}
+	});
+	chrome.storage.local.get('yuyueTime', function(result) {
+		console.log(result.yuyueTime);
+		if (0 != result.yuyueTime) {
+			document.getElementById('time').value = result.yuyueTime;
+		} else {
+			document.getElementById('time').value = 1000;
 		}
 	});
 	
-
+	//是否开启自动抢购
 	document.getElementById('checbox').onclick = function() {
 		var vv = document.getElementById('checbox').checked;
-		chrome.storage.local.get('isAuto', function(result) {
-			console.log(result.isAuto);
-		});
 		
 		if (true == vv) {
 			chrome.storage.local.set({'isAuto': 10}, function() {
 				console.info('存储成功');
 			});
+			document.getElementById('zidong').style.display = 'block';
 		} else {
 			chrome.storage.local.set({'isAuto': 99}, function() {
 				console.info('存储成功');
 			});
+			document.getElementById('zidong').style.display = 'none';
+			document.getElementById('autoRechecbox').checked = false;
+			chrome.storage.local.set({'isAutoRe': 99}, function() {
+				console.info('存储成功');
+			});
 		}
+	};
+
+	//预约刷新
+	document.getElementById('autoRechecbox').onclick = function() {
+		var vv = document.getElementById('autoRechecbox').checked;
+		var time = document.getElementById('time').value;
+		if (true == vv) {
+			chrome.storage.local.set({'isAutoRe': 10}, function() {
+				chrome.storage.local.set({'yuyueTime': time}, function() {});
+				console.info('存储成功');
+			});
+		} else {
+			chrome.storage.local.set({'isAutoRe': 99}, function() {
+				chrome.storage.local.set({'yuyueTime': 0}, function() {});
+				console.info('存储成功');
+			});
+		}
+	};
+
+	//预约刷新
+	document.getElementById('time').onchange = function() {
+		document.getElementById('autoRechecbox').checked = false;
+		chrome.storage.local.set({'isAutoRe': 99}, function() {
+			chrome.storage.local.set({'yuyueTime': 0}, function() {});
+			console.info('存储成功');
+		});
 	};
 });
